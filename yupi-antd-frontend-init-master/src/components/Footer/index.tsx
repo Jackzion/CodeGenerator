07cb9b791c1496c7111d5b9fd93fdb01e8f67c02
ziio. <1934255852@qq.com>
2024-11-21 19:02:22 +0800

    CodeGenerator-forend Init

---
+++ yupi-antd-frontend-init-master/src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const defaultMessage = 'zeenoh jj';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: (
+            <>
+              <GithubOutlined /> CodeGenerator SourceCode
+            </>
+          ),
+          href: 'https://github.com/Jackzion/CodeGenerator',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
